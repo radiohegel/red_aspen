66b864d3503b05e2c38b6e6d467a5c2773820f1c
Roger <roger.e.clayton@gmail.com>
2018-01-05 17:07:48 -0800

    adding about section text - playing with sizing, content

--- app/javascript/frontend_client/pages/HomePage.vue
+++ app/javascript/frontend_client/pages/HomePage.vue
@@ -3,8 +3,22 @@
 		<parallax :fixed="true">
 			<header-banner></header-banner>
 		</parallax>
+		<section class="section is-large">
+			<div class="content">
+				<p class="title is-3  has-text-left">
+					Red Aspen Management is a real estate investment company based in Utah. A family company, founded in 2016, we purchase, restore, manage, and sell properties throughout Salt Lake and Davis counties. 
+				</p>				
+				<br>
+				<br>
+				<p class="title is-2  has-text-left has-text-primary">
+					If you are looking to sell your home, you have come to the right place!
+				</p>				
+			</div>
+		</section>
 		<section class="hero is-fullheight is-white">
-			<div class="hero-body"></div>
+			<div class="hero-body">
+				
+			</div>
 		</section>
 	</div>
 </template>
@@ -28,5 +42,8 @@
 
 <style lang="sass" scoped>
 	@import "../styles/app.sass"
+
+	.section
+		background: white
 
 </style>
