359a0df5a1e06e0a8e74e4ff093aacc9504d8207
Roger <roger.e.clayton@gmail.com>
2018-01-06 22:20:01 -0800

    playing with columns - probably to be scrapped

--- app/javascript/frontend_client/components/AboutSection.vue
+++ app/javascript/frontend_client/components/AboutSection.vue
@@ -1,9 +1,18 @@
 <template>
-	<section class="hero is-fullheight is-white">
+	<section class="hero is-medium is-white">
 		<div class="hero-body content">
-			<p class="title is-3  has-text-left">
-				Red Aspen Management is a real estate investment company based in Utah. A family company, founded in 2016, we purchase, restore, manage, and sell properties throughout Salt Lake and Davis counties. 
-			</p>							
+			<div class="columns">
+  			<div class="column is-half">
+					<p class="title is-3  has-text-right has-text-primary">
+						Red Aspen Management is a Utah-based real estate investment company. Family owned and operated, we purchase, restore, manage, and sell properties in the Greater Salt Lake area. 
+					</p>	
+  			</div>
+				<div class="column is-half">
+					<p class="title is-3 has-text-left">
+						If you are looking to sell your home in Salt Lake or Davis County, Utah, you have come to the right place!
+					</p>
+				</div>
+			</div>
 		</div>
 	</section>
 </template>
@@ -23,5 +32,8 @@
 
 <style lang="sass" scoped>
 	@import "../styles/app.sass"
+
+	.columns
+		padding: 7%
 
 </style>
