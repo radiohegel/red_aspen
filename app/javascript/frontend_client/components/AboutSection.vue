91bce0468b18551b57f96d8662f1fca5bc72d1a1
Roger <roger.e.clayton@gmail.com>
2018-01-08 18:56:33 -0800

    minor update to content phrasing

--- app/javascript/frontend_client/components/AboutSection.vue
+++ app/javascript/frontend_client/components/AboutSection.vue
@@ -2,7 +2,7 @@
 	<section class="hero is-medium is-white">
 		<div class="hero-body content">
 			<p class="title is-2  has-text-centered has-text-primary">
-				Red Aspen Management is a Utah-based real estate investment company. Family owned and operated, we purchase, restore, manage, and sell properties throughout Salt Lake and Davis Counties.
+				Red Aspen Management is a Utah-based real estate investment company. Family owned and operated, we purchase, restore, manage, and sell properties throughout the Salt Lake Valley.
 			</p>				
 			<p class="title is-2  has-text-centered">
 				If you are looking to sell your home, you have found the right place!
