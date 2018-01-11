8ea7875f2bb7d74cabe0edbd9ada103dbd0381e0
Roger <roger.e.clayton@gmail.com>
2018-01-10 22:04:08 -0800

    adds mountain svg

--- app/javascript/frontend_client/components/AboutSection.vue
+++ app/javascript/frontend_client/components/AboutSection.vue
@@ -1,13 +1,16 @@
 <template>
 	<div>
 	<section class="hero is-medium is-white">
+		<div class="hero-head">
+			<mountains/>
+		</div>
 		<div class="hero-body content">
 			<p class="title is-2  has-text-centered has-text-primary">
 				Red Aspen Management is a Utah-based real estate investment company. Family owned and operated, we purchase, restore, manage, and sell properties throughout the Salt Lake Valley.
 			</p>
 		</div>
 	</section>
-	<section class="hero">
+	<section class="hero is-primary">
 		<div class="hero-body">
 			<carousel :perPage="1">
 			  <slide>
@@ -59,11 +62,12 @@
 
 <script>
 	import {Carousel, Slide} from 'vue-carousel'
+	import Mountains from '../svg/mountains.svg'
 
 	export default {
 
 	  name: 'AboutSection',
-	  components: { Carousel, Slide },
+	  components: { Carousel, Slide, Mountains },
 	  data () {
 	    return {
 
